b60c4059378def121678bae1b1253c6f9c2f5c95
Odomontois <odomontois@gmail.com>
2016-01-19 14:46:51 +0300

    Params are reading

--- Main.hs
+++ Main.hs
@@ -4,7 +4,57 @@ module Main where
 
 import Text.XML.HXT.Core
 import WordProcessing
-import System.Environment
+import System.Environment 
+
+content::Process
+content = styles <+> body where
+  styles = readDocument [] "styles.xml" >>> getChildren
+
+  body = we "body" (getChildren >>> clifDocu)
+
+  normal = paragraph "Normal"
+
+  clifDocu =
+    isElem
+    >>> (hasName "class" <+> hasName "interface")
+    >>> clifHeading <+> descr <+> methods
+
+  clifHeading = paragraph "Heading1" text where 
+    text          = name >>> mkText 
+    name          = kind <+> getAttrValue "name"
+    kind          = kindClass <+> kindInterface
+    kindClass     = hasName "class"     >>> constA "Класс "
+    kindInterface = hasName "interface" >>> constA "Интерфейс "
+
+  methods = 
+    getChildren
+    >>> isElem
+    >>> hasName "method"
+    >>> methodHeading <+> descr <+> params
+
+  methodHeading = paragraph "Heading2" text where
+    text = constA "метод " <+> getAttrValue "name" >>> mkText
+
+  fieldVal name = 
+    getChildren 
+    >>> isElem
+    >>> hasName name 
+    >>> getChildren 
+    >>> isText
+
+  descr  = normal  $ fieldVal "description"
+
+  params = wtbl "TableNormal" widths borders rows where
+    borders = wtblBorders 4
+    widths = [100, 200, 400]
+    rows = 
+      getChildren 
+      >>> isElem
+      >>> hasName "param"
+      >>> we "tr" cells
+    cells = name <+> we "tc" descr 
+    name = we "tc" $ paragraph "Bold" $ getAttrValue "name" >>> mkText
+
 
 readParams::IO (String, String)
 readParams = do 
@@ -16,21 +66,13 @@ readParams = do
     [src, dst] -> (src, dst)
     _other     -> (defaultSrc, defaultDst)
 
-instance WordProcessing (IOSLA s) where
-  processChild =
-    isElem
-    >>> (hasName "class" <+> hasName "interface")
-    >>> paragraph name
-    where 
-      paragraph =  we "p" . we "r" . we "t" 
-      name = getAttrValue "name" >>>  mkText
 
 main::IO ()
 main = do
   (src, dst) <- readParams
   _ <- runX $
     readDocument [withValidate no] src
-    >>> root [] [ getChildren >>> isElem >>> hasName "types" >>> result]
+    >>> root [] [ getChildren >>> isElem >>> hasName "types" >>> result content]
     >>> writeDocument [withIndent yes] dst 
   return ()
 
--- WordProcessing.hs
+++ WordProcessing.hs
@@ -5,23 +5,53 @@ import Text.XML.HXT.Core
 wordNS::String
 wordNS = "http://schemas.microsoft.com/office/word/2003/wordml"
 
-type Process a = a XmlTree XmlTree
+type Process = IOStateArrow () XmlTree XmlTree
 
-class ArrowXml a => WordProcessing a where
 
-  w::String->[Process a]->[Process a]->Process a
-  w = mkqelem . flip (mkQName "w") wordNS
+w::String->[Process]->[Process]->Process
+w = mkqelem . flip (mkQName "w") wordNS
 
-  we::String->Process a->Process a
-  we name child = w name [] [child] 
+we::String->Process->Process
+we name child = w name [] [child] 
 
-  wa::String->String->Process a
-  wa = sqattr . flip (mkQName "w") wordNS
+wa::String->String->Process
+wa = sqattr . flip (mkQName "w") wordNS
 
-  result::Process a->Process a
-  result content = w "wordDocument" [sattr "xmlns:w" wordNS] [content]
-    -- where
-    --   children = getChildren >>> processChild
-    --   body = we "body" 
+result::Process->Process
+result content = w "wordDocument" [sattr "xmlns:w" wordNS] [content]
+
+paragraph::String->Process->Process
+paragraph style text = w "p" [] [
+  we "pPr" $ w "pStyle" [wa "val" style] [],
+  we  "r"  $ we "t"  text 
+  ]  
+
+wtbl::String->[Integer]->Process->Process->Process
+wtbl style widths props rows = table where
+  table = w "tbl" [] [
+    w "tblPr" [] [
+      w "tblStyle" [wa "val" style] [],
+      props], 
+    we "tblGrid" cols,
+    rows
+    ]
+  gridCol width = w "gridCol" [wa "w" $ show width] []
+  cols = catA $ map gridCol widths
+
+wtblBorders::Integer->Process
+wtblBorders size = w "tblBorders" [] $ map border sides where
+  sides = ["top", "left", "bottom", "right", "insideH", "insideV"]
+  border side = 
+    w side [
+      wa "val"   "single",
+      wa "sz"    $ show size,
+      wa "space" "0",
+      wa "color" "auto"
+      ] []
+
+
+
+
+
 
 
